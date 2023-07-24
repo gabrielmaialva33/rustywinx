9f4459d5cf34f58ac5b7bbadebb91b13fc1c7063
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-07-24 16:23:24 -0300

    chore: add teloxide lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+
+use teloxide::prelude::*;
+use log::info;
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    pretty_env_logger::init();
+
+    info!("Starting bot...");
+
+    let bot = Bot::new(env::var("BOT_TOKEN").unwrap());
+
+    teloxide::repl(bot, |bot: Bot, message: Message| async move {
+        bot.send_dice(message.chat.id).await?;
+        Ok(())
+    }).await;
 }
