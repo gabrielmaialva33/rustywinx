9d7b93986c6ae45b9e9cd95c84289a14cbdcfbd7
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-07-25 13:23:20 -0300

    feat: add start command

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
+mod commands;
+mod handlers;
+
 extern crate dotenv;
 
 use dotenv::dotenv;
 use log::info;
 use std::env;
-use teloxide::prelude::*;
+use std::error::Error;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
-    pretty_env_logger::init();
+    pretty_env_logger::init_timed();
 
     info!("Starting bot...");
 
     let bot = Bot::new(env::var("BOT_TOKEN").unwrap());
 
-    teloxide::repl(bot, |bot: Bot, message: Message| async move {
-        bot.send_dice(message.chat.id).await?;
-        Ok(())
-    })
-    .await;
+    let handler = dptree::entry()
+        .branch(Update::filter_message().endpoint(handlers::message::message_handler));
+
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+
+    Ok(())
+}
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    #[command(description = "Display this text.")]
+    Start,
 }
